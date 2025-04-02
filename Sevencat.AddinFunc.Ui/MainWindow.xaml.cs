8f4d0544335005361f3ebf88ec02739d7f35ce10
sevencat <bastet.wang@aliyun.com>
2025-04-02 16:57:59 +0800

    增加了一个服务器，用来做http处理。

---
+++ Sevencat.AddinFunc.Ui/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using Nancy.Hosting.Self;
+
+namespace Sevencat.AddinFunc.Ui
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var hostConfigs = new HostConfiguration
+			{
+				UrlReservations = new UrlReservations() { CreateAutomatically = true },
+				MaximumConnectionCount = 2,
+			};
+			var uri = new Uri("http://localhost:9800");
+			_host = new NancyHost(hostConfigs, uri);
+
+			_host.Start();
+		}
+
+		private NancyHost _host;
+	}
+}
---
+++ Sevencat.AddinFunc.Ui/Service/TestService.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+
+namespace Sevencat.AddinFunc.Ui.Service;
+
+public class TestService : NancyModule
+{
+	public TestService() : base("/api/test")
+	{
+		Get("/hello", _ => "Hello from Nancy");
+	}
+}
